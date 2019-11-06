7d1e0f71608340280564f65cd65a180a35ff0aa0
ErnestoJM <ernesjimenez@hotmail.com>
2019-11-06 02:41:39 +0100

    new website

---
+++ mainScript.js
@@ -0,0 +1,20 @@
+$(document).on("ready", configApp);
+
+function configApp()
+{
+    var canvas = document.getElementById("miCanvas");
+    var ctx = canvas.getContext("2d");
+    canvas.width = screen.availWidth;
+    drawFooter(canvas, ctx);
+
+}
+
+function drawFooter(canvas, ctx){
+
+    ctx.fillStyle = "rgba(0,0,0,0.8)"
+    ctx.moveTo(0,0);
+    ctx.quadraticCurveTo(0, -50, canvas.width - 100, canvas.height - 50)
+    ctx.fill();
+
+    //quadraticCurveTo(cpx, cpy, x, y)
+}
